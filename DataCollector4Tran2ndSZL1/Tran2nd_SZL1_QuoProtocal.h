a4024040951709902d2ffcd6b74f8807a8174517
BarryPaneer <barry.paneer@qq.com>
2017-08-13 23:20:59 +0800

    updates

--- DataCollector4Tran2ndSZL1/Tran2nd_SZL1_QuoProtocal.h
+++ DataCollector4Tran2ndSZL1/Tran2nd_SZL1_QuoProtocal.h
@@ -5,34 +5,14 @@
 
 typedef struct
 {
-	char						Code[20];					///< 合约代码
-	char						ContractID[20];				///< 合约代码
-	char						Name[64];					///< 合约名称
-	unsigned char				Kind;						///< 证券类型
-	unsigned char				DerivativeType;				///< 衍生品类型：欧式美式+认购认沽
-	char						UpdateVersion;		//期权合约的版本号(新挂合约是'1')
-	unsigned int				LotSize;					///< 一手等于几张合约
-	char						UnderlyingCode[20];			///< 标的证券代码
-	unsigned int				ContractUnit;				///< 合约单位(经过除权除息调整后的合约单位，一定为整数)
-	unsigned int				XqPrice;					///< 行权价格[*放大倍数]
-	unsigned int				StartDate;					///< 首个交易日(YYYYMMDD)
-	unsigned int				EndDate;					///< 最后交易日(YYYYMMDD)
-	unsigned int				XqDate;						///< 行权日(YYYYMM)
-	unsigned int				DeliveryDate;				///< 交割日(YYYYMMDD)
-	unsigned int				ExpireDate;					///< 到期日(YYYYMMDD)
-	char						StatusFlag[8];		//期权合约状态(8个字符,详细定义见文档)
-} tagSHOptReferenceData_LF108;
-
-
-typedef struct
-{
-	char						Key[32];					///< 索引键值
+	char						Key[20];					///< 索引键值
 	unsigned int				MarketID;					///< 市场编号
+	unsigned int				MarketDate;					///< 市场日期
 	unsigned int				KindCount;					///< 类别数量
 	unsigned int				WareCount;					///< 商品数量
 	unsigned int				PeriodsCount;				///< 交易时段信息列表长度
 	unsigned int				MarketPeriods[8][2];		///< 交易时段描述信息列表
-} tagSHOptMarketInfo_LF109;
+} tagSZL1MarketInfo_LF164;
 
 
 typedef struct
@@ -42,21 +22,47 @@ typedef struct
 	unsigned int				PriceRate;					///< 价格放大倍数[10的多少次方]
 	unsigned int				LotSize;					///< “手”比率
 	unsigned int				WareCount;					///< 该分类的商品数量
-} tagSHOptKindDetail_LF110;
+} tagSZL1KindDetail_LF165;
+
+
+typedef struct
+{
+	char						Key[20];					///< 索引键值
+	unsigned int				MarketTime;					///< 市场时间
+	unsigned char				MarketStatus;				///< 市场状态[0初始化 1行情中]
+} tagSZL1MarketStatus_HF166;
 
 
 typedef struct
 {
 	char						Code[20];					///< 合约代码
+	char						ContractID[20];				///< 合约代码
+	char						Name[64];					///< 合约名称
+	unsigned char				Kind;						///< 证券类型
+} tagSZL1ReferenceData_LF163;
+
+
+typedef struct
+{
+	char						Code[20];					///< 名称代码表
+	unsigned char				StopFlag;					///< 停牌标记，是否可交易，需要推送
+	unsigned char				PLimit;						///< 涨、跌停是否有效
+	unsigned int				Worth;						///< 每股面值，*放大倍率10000
+	unsigned int				ExTts;						///< 交易单位
+	unsigned int				MaxExVol;					///< 最大交易数量
+	unsigned int				MinExVol;					///< 最小交易数量
+} tagSZL1ReferenceExtension_LF164;
+
+
+typedef struct
+{
+	char						Code[20];					///< 合约代码
+	unsigned int				PreClose;					///< 昨收价[*放大倍数]
 	unsigned int				Open;						///< 开盘价[*放大倍数]
 	unsigned int				Close;						///< 今收价[*放大倍数]
-	unsigned int				PreClose;					///< 昨收价[*放大倍数]
-	unsigned int				UpperPrice;					///< 当日涨停价格[*放大倍数], 0表示无限制
-	unsigned int				LowerPrice;					///< 当日跌停价格[*放大倍数], 0表示无限制
-	unsigned int				SettlePrice;				///< 今结价[*放大倍数]
-	unsigned int				PreSettlePrice;				///< 合约昨结[*放大倍数]
-	unsigned __int64			PreOpenInterest;			///< 昨日持仓量(张)
-} tagSHOptSnapData_LF112;
+	unsigned int				HighLimit;					///< 涨停限价[*放大倍数]
+	unsigned int				LowLimit;					///< 跌停限价[*放大倍数]
+} tagSZL1SnapData_LF165;
 
 
 typedef struct
@@ -68,31 +74,22 @@ typedef struct
 	double						Amount;						///< 总成交金额[元]
 	unsigned __int64			Volume;						///< 总成交量[股/张]
 	unsigned __int64			Position;					///< 持仓量
-} tagSHOptSnapData_HF113;
+} tagSZL1SnapData_HF166;
 
 
 typedef struct
 {
 	unsigned int				Price;						///< 委托价格[* 放大倍数]
 	unsigned __int64			Volume;						///< 委托量[股]
-} tagBuySellItem;
+} tagSZL1BuySellItem;
 
 
 typedef struct
 {
 	char						Code[20];					///< 合约代码
-	tagBuySellItem				Buy[5];						///< 买五档
-	tagBuySellItem				Sell[5];					///< 卖五档
-} tagSHOptSnapBuySell_HF114;
-
-
-typedef struct
-{
-	char						Key[32];					///< 索引键值
-	unsigned int				MarketDate;					///< 市场日期
-	unsigned int				MarketTime;					///< 市场时间
-	unsigned char				MarketStatus;				///< 市场状态[0初始化 1行情中]
-} tagSHL1MarketStatus_HF105;
+	tagSZL1BuySellItem			Buy[5];						///< 买五档
+	tagSZL1BuySellItem			Sell[5];					///< 卖五档
+} tagSZL1SnapBuySell_HF167;
 
 
 
